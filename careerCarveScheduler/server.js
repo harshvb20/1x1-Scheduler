e676286d3ae97901633f4dd4537cc2340892fbef
harshvb20 <harshvb20@iitk.ac.in>
2024-08-16 00:41:08 +0530

    update

--- careerCarveScheduler/server.js
+++ careerCarveScheduler/server.js
@@ -66,11 +66,11 @@ app.post('/schedule', (req, res) => {
 });
 
 // Serve static files from the frontend build directory
-app.use(express.static(path.join(__dirname, 'careercarvefrontend/')));
+app.use(express.static(path.join(__dirname, 'careercarvefrontend/src')));
 
 // Route all other requests to the frontend
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'careercarvefrontend/', 'App.js'));
+    res.sendFile(path.join(__dirname, 'careercarvefrontend/src', 'App.js'));
 });
 
 // Root endpoint
